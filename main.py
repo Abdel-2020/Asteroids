97bf007a70c9898bbc5a1d9aa3acd52367563199
abdel <abdelnoura97@gmail.com>
2026-01-20 22:28:25 +0000

    Asteroids spawning in

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -10,7 +12,17 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # Creating Groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable) 
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
     
+    asteroid_field = AsteroidField()
     # Instantiating player object
     player = Player((SCREEN_WIDTH/2),(SCREEN_HEIGHT/2))
 
@@ -22,15 +34,17 @@ def main():
         log_state()
         for event in pygame.event.get():
             
-            screen.fill("black")
-
-            player.update(dt)
-            player.draw(screen)
-
-            pygame.display.flip()
-            
+                        
             if event.type == pygame.QUIT:
                 return
+        screen.fill("black")
+        updatable.update(dt)
+
+        for sprite in drawable:
+            sprite.draw(screen)
+
+        pygame.display.flip()
+
         dt = (clock.tick(60) / 1000) 
 
 if __name__ == "__main__":
