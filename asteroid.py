039f2b8bb16dc047ab9476343cb55e89863077e8
abdel <abdelnoura97@gmail.com>
2026-01-21 13:04:44 +0000

    course completed!

--- asteroid.py
+++ asteroid.py
@@ -1,11 +1,12 @@
 import pygame
+import random
 from circleshape import CircleShape
-from constants import LINE_WIDTH
+from constants import LINE_WIDTH, ASTEROID_MIN_RADIUS
+from logger import log_event
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
-    pass
 
     def draw(self, screen):
         pygame.draw.circle(screen, "white", self.position, self.radius, LINE_WIDTH)
@@ -13,4 +14,24 @@ class Asteroid(CircleShape):
     def update(self, dt):
         self.position += self.velocity * dt
 
+    def split(self):
+        self.kill()
+
+        if self.radius < ASTEROID_MIN_RADIUS:
+            return
         
+        # if the asteroid is large enough to split, log the event
+        log_event("asteroid_split")
+
+        split_angle = random.uniform(20, 50)
+
+        first_asteroid_split = self.velocity.rotate(split_angle)
+        second_asteroid_split = self.velocity.rotate(-split_angle)
+
+        new_asteroid_radius = self.radius - ASTEROID_MIN_RADIUS
+     
+        new_asteroid_one = Asteroid(self.position.x,self.position.y, new_asteroid_radius)
+        new_asteroid_two = Asteroid(self.position.x, self.position.y, new_asteroid_radius)
+
+        new_asteroid_one.velocity = first_asteroid_split * 1.2
+        new_asteroid_two.velocity = second_asteroid_split * 1.2
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from logger import log_state, log_event
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -18,10 +19,12 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable) 
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
+    Shot.containers = (shots, drawable, updatable)
     
     asteroid_field = AsteroidField()
     # Instantiating player object
@@ -45,7 +48,12 @@ def main():
                 log_event("player_hit") 
                 print("Game Over!")
                 sys.exit()
-                
+        for asteroid in asteroids:
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    log_event("asteroid_shot")
+                    shot.kill()
+                    asteroid.split()
 
         for sprite in drawable:
             sprite.draw(screen)
